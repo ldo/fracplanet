f7d1cf308e68ca91b04ea548da4981971148ce19
timday <timday@b61c5637-9d11-496f-99c9-1cb6e07b56bf>
2017-11-13 15:47:49 +0000

    Oops tagged in wrong place.  Compiles on Debian/Stretch's g++.
    git-svn-id: svn://svn.code.sf.net/p/fracplanet/code/trunk@303 b61c5637-9d11-496f-99c9-1cb6e07b56bf

--- image.cpp
+++ image.cpp
@@ -64,7 +64,7 @@ template <> bool Raster<uchar>::write_pgmfile(const std::string& filename,Progre
       out.write(reinterpret_cast<const char*>(&(*(row->begin()))),row->size());
     }
   out.close();
-  return out;
+  return static_cast<bool>(out);
 }
 
 template <> bool Raster<ushort>::write_pgmfile(const std::string& filename,Progress* target) const
@@ -94,7 +94,7 @@ template <> bool Raster<ushort>::write_pgmfile(const std::string& filename,Progr
 	}
     }
   out.close();
-  return out;
+  return static_cast<bool>(out);
 }
 
 template <> bool Raster<ByteRGBA>::write_ppmfile(const std::string& filename,Progress* target) const
@@ -111,7 +111,7 @@ template <> bool Raster<ByteRGBA>::write_ppmfile(const std::string& filename,Pro
 	out.write(reinterpret_cast<const char*>(&((*it).r)),3);
     }
   out.close();
-  return out;
+  return static_cast<bool>(out);
 }
 
 
